2d2aeb659ea21d153ce188bf7d40abd693e7f3a3
zumii780 <152663791+zumii780@users.noreply.github.com>
2025-09-29 12:35:56 -0300

    Add files via upload
    resuelta la implementación del programa 
    falta hacer un main 
    anashe

---
+++ implementacion.hs
@@ -0,0 +1,24 @@
+import Data.List (sortBy)
+
+type Intervalo = (Int, Int)
+
+compararIntervalo :: Intervalo -> Intervalo -> Ord
+compararIntervalo (_, b1) (_, b2) = compare b1 b2
+
+seleccionarIntervalos :: [Intervalo] -> [Intervalo]
+seleccionarIntervalos [] = []
+seleccionarIntervalos intervalos =
+    reverse (go (tail intervalosOrdenados) [head intervalosOrdenados])
+  where
+    intervalosOrdenados = sortBy compararIntervalo intervalos
+
+    go :: [Intervalo] -> [Intervalo] -> [Intervalo]
+    go [] acumulador = acumulador
+    go (intervaloActual:resto) (ultimoSeleccionado:acumuladorResto) =
+      let inicioActual     = fst intervaloActual
+          finUltimo        = snd ultimoSeleccionado
+      in
+        if inicioActual >= finUltimo then
+          go resto (intervaloActual : ultimoSeleccionado : acumuladorResto)
+        else
+          go resto (ultimoSeleccionado : acumuladorResto)
